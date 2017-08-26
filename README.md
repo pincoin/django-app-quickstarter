76b0ebfbe1aa675058ebc5c98102523f30b74523
pincoin <pincoins@gmail.com>
2017-08-26 16:51:32 +0900

    - forms.py added - README updated

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # django-app-quickstarter
 django app quickstarter
+
+# DO NOT FORGET TO CHANGE!
+1. Please, change the app directory name: `barebone`.
+
+[barebone](barebone)
+
+2. Please, change the inherited class of `AppConfig`.
+
+[barebone](barebone/apps.py)
+
+3. Please, change the package name in [setup.py](setup.py)
+
+[setup.py#L9](setup.py#L9)
+
+# Coverage, Tox supported
+
+# Package build and installation
+## Build
+```
+python setup.py sdist
+```
+
+## Install
+```
+python setup.py install -f
+```
--- setup.py
+++ setup.py
@@ -5,40 +5,41 @@ from setuptools import setup, find_packages
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
-setup(name='django-app-quickstarter',
-      version='0.1',
-      packages=find_packages(exclude=['docs', 'tests']),
-      description='Example django app package for setuptools, tox and coverage',
-      long_description=README,
-      url='https://www.pincoin.info/',
-      author='John Doe',
-      author_email='mairoo' '@' 'pincoin.info',
-      license='MIT',
+setup(
+    name='django-app-quickstarter',
+    version='0.1',
+    packages=find_packages(exclude=['docs', 'tests']),
+    description='Example django app package for setuptools, tox and coverage',
+    long_description=README,
+    url='https://www.pincoin.info/',
+    author='John Doe',
+    author_email='mairoo' '@' 'pincoin.info',
+    license='MIT',
 
-      # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-          'Development Status :: 3 - Alpha',
-          'Intended Audience :: Developers',
-          'Topic :: Software Development :: Build Tools',
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6',
-      ],
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
 
-      install_requires=[
-      ],
-      setup_requires=[
-      ],
-      scripts=[
-      ],
+    install_requires=[
+    ],
+    setup_requires=[
+    ],
+    scripts=[
+    ],
 
-      test_suite='runtests.runtests',
-      tests_require=[
-      ],
+    test_suite='runtests.runtests',
+    tests_require=[
+    ],
 
-      zip_safe=False,
-      ),
+    zip_safe=False,
+),
